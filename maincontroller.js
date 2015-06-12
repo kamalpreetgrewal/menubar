5c21abf37f77c8f41207275cfb11481038f89ca6
kamalpreetgrewal <grewalkamal005@gmail.com>
2015-06-12 15:00:35 +0530

    implements ng-click on menu options

--- maincontroller.js
+++ maincontroller.js
@@ -1,3 +1,21 @@
 menubar.controller('MainController',['$scope',function($scope){
   $scope.title='Sample text written using AngularJS';
+  $scope.menuoptions=
+    [
+      {
+        name: 'Home'
+      },
+      {
+        name: 'Features'
+      },
+      {
+        name: 'Pricing'
+      },
+      {
+        name: 'Sign Up'
+      }
+    ];
+  $scope.display=function(index) {
+                   alert($scope.menuoptions[index].name);
+  };
 }]);
